d78a032e1ad8bf47feacb4c5386b40123dd3743d
Emilyb261 <ebillings@unomaha.edu>
2025-11-16 13:38:43 -0600

    Add turbine data plotting functionality

--- main.py
+++ main.py
@@ -1,4 +1,29 @@
-#MapPlot.py
-#Name:
-#Date:
-#Assignment:
+MapPlot.py
+# Name:
+# Date:
+# Assignment:
+
+import wind_turbines
+import pandas as pd 
+import matplotlib.pyplot as plt 
+turbines = wind_turbines.get_turbines()
+
+print(turbines[0]["Data"]["Number_Turbines"]["Total"])
+years = []
+totals = []
+for turbine in turbines:
+    year = turbine["Year"] 
+    total = turbine["Data"]["Number_Turbines"]["Total"] 
+    if total != 0: 
+        years.append(year) 
+        totals.append(total)
+    #print(year, total)
+df = pd.DataFrame({"Year": years, "Total": totals})
+
+print(df) 
+#df.plot(kind = 'scatter', x = 'Year', y = 'Total')
+plt.plot(years, totals, 'ro')
+plt.xlabel("Year")
+plt.title("Number of Turbines over time")
+plt.savefig("Output.png") 
+plt.close() 
